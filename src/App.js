9e5efd957bdb98e1d666db773a77cc22d7a3e8b0
Drei <stoicescuofsuburbia@gmail.com>
2019-04-08 17:48:17 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import jsonServiceProvider from 'ra-data-json-server';
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
-import PostList, { PostEdit, PostCreate } from './posts';
-import UserList from './users';
+import dataProvider from './dataProvider';
+import EventList from './events';
 import Dashboard from './Dashboard';
-import authProvider from './authProvider';
-
-const dataProvider = jsonServiceProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin
-    dashboard={Dashboard}
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-  >
-    <Resource
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource list={EventList} name="events" />
+    {/* <Resource
       create={PostCreate}
       edit={PostEdit}
-      icon={PostIcon}
       list={PostList}
       name="posts"
-    />
-    <Resource name="users" list={UserList} icon={UserIcon} />
+    /> */}
   </Admin>
 );
 
