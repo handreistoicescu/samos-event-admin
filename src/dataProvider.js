20e3d3a140e073358bb1f6f78afa685c72d83345
Drei <stoicescuofsuburbia@gmail.com>
2019-04-27 16:39:47 +0300

    Fix pagination

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -95,12 +95,23 @@ const convertHTTPResponseToDataProvider = (
   params
 ) => {
   const { headers, json } = response;
-
+  console.log(type, resource, headers.get('Content-Range'));
   switch (type) {
     case GET_LIST:
+      if (!headers.has('content-range')) {
+        throw new Error(
+          'The Content-Range header is missing in the HTTP Response. The simple REST data provider expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare Content-Range in the Access-Control-Expose-Headers header?'
+        );
+      }
       return {
         data: json[resource],
-        total: parseInt(headers.get('Content-Range'), 10)
+        total: parseInt(
+          headers
+            .get('content-range')
+            .split('/')
+            .pop(),
+          10
+        )
       };
     case GET_MANY:
       return {
@@ -127,7 +138,7 @@ export default (type, resource, params) => {
     resource,
     params
   );
-  return fetchJson(url, options).then(response =>
-    convertHTTPResponseToDataProvider(response, type, resource, params)
-  );
+  return fetchJson(url, options).then(response => {
+    return convertHTTPResponseToDataProvider(response, type, resource, params);
+  });
 };
